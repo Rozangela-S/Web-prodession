991f73e35ab3679f88e348761b3eb65ce0bc06f9
Rozangela <rozangelalucas155@gmail.com>
2025-03-05 20:23:03 -0300

    feat: creation endpoint header

--- src/app/header/header.tsx
+++ src/app/header/header.tsx
@@ -1,16 +1,15 @@
-import { ShimmerButton } from "@/components/magicui/shimmer-button";
+import Link from "next/link";
 
 export default function Header() {
   return(
-    <div className="w-full h-32 bg-gray-100 ">
-      <header className="flex justify-end">
-        <nav className="flex gap-4">
-          <ShimmerButton>Sobre nós</ShimmerButton>
-          <ShimmerButton>Quiz</ShimmerButton>
-          <ShimmerButton>saida</ShimmerButton>
+    <header className="w-full bg-green-500 text-white">
+      <div className="container mx-auto flex items-center justify-between px-6 py-3">
+        <nav className="hidden md:flex items-center justify-between gap-6 text-sm">
+          <Link href="/" >Home</Link>
+          <Link href="/profissoes">Profissões</Link>
+          <Link href="tendencias">Tendências</Link>
         </nav>
-      </header>
-    </div>
-    
+      </div>
+    </header>
   )
 }
