9b8b52a61cd8f4af7f364e055968b235460676ca
Rozangela <rozangelalucas155@gmail.com>
2025-03-09 23:10:03 -0300

    feat: ajuste e refatoração de componentes

--- src/app/header/header.tsx
+++ src/app/header/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return(
     <header className="w-full bg-green-500 text-white">
       <div className="container mx-auto flex items-center justify-between px-6 py-3">
-        <nav className="hidden md:flex items-center justify-between gap-6 text-sm">
+        <nav className="hidden md:flex items-center justify-between gap-6 text-xl">
           <Link href="/" >Home</Link>
           <Link href="/profissoes">Profissões</Link>
           <Link href="/tendencias">Tendências</Link>
